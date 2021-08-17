785ba5a339da2112e447cc230ea6dc2919b23cc4
MARIE VALDOVINOS <MEVALDOVI@GMAIL.COM>
2021-08-17 15:35:31 -0500

    edited viewRoutes.js

--- server.js
+++ server.js
@@ -4,20 +4,21 @@ const logger = require("morgan");
 const path = require("path");
 const db = require("./Develop/models/Workout");
 const routes = require("./Develop/controllers");
-const viewRoutes = require("./Develop/controllers/view/viewRoutes");
+const viewRoutes = require("./Develop/controllers/view/viewRoutes.js");
 const api = require("./Develop/controllers/api/apiRoutes");
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.use(viewRoutes);
+//use the routes
 //the 'api' below works but for some weird reason, 'viewRoutes' will not!
 app.use(api);
+app.use(viewRoutes);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
+app.use(express.static("./Develop/public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
     useNewUrlParser: true,
@@ -26,7 +27,8 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
 });
 
 // routes
-app.use(require("./Develop/controllers/api"));
+// app.use(require("./Develop/controllers/api"));
+// app.use(require("./Develop/controllers/view"));
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);

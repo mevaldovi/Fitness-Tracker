3ea54b42d055a086b9a148bf70fd23e0f09a3a5e
MARIE VALDOVINOS <MEVALDOVI@GMAIL.COM>
2021-08-16 20:26:51 -0500

    updated routes variables in server.js

--- server.js
+++ server.js
@@ -3,9 +3,15 @@ const mongoose = require("mongoose");
 const logger = require("morgan");
 const path = require("path");
 const db = require("./Develop/models")
+const routes = require("./Develop/controllers");
+const view = require("./Develop/controllers/view/viewRoutes");
+const api = require("./Develop/controllers/api/apiRoutes");
 
 const PORT = process.env.PORT || 3000;
 const app = express();
+
+app.use(viewRoutes);
+app.use(apiRoutes);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());

21df8bf6eb7607962b051f83385ce5e6142b03ab
MARIE VALDOVINOS <MEVALDOVI@GMAIL.COM>
2021-08-16 21:53:47 -0500

    updated api routes

--- Develop/controllers/api/apiRoutes.js
+++ Develop/controllers/api/apiRoutes.js
@@ -6,13 +6,37 @@ const router = require("express").Router();
 const index = require("../api/index");
 const Workout = require("../../models/Workout");
 
-//route to get all the workouts
+//route to get all the workouts out from the db
 router.get("api/workouts", (req, res) => {
     Workout.aggregate([{
-        $addFields: {
-            totalDuration: {
-                $sum: "workout.duration",
+            $addFields: {
+                totalDuration: {
+                    $sum: "workout.duration",
+                },
             },
-        },
-    }])
+        }])
+        .then((fitnesstrackerdb) => {
+            return res.json(fitnesstrackerdb);
+        })
+        .catch((err) => {
+            res.json(err);
+        });
 });
+
+//create a workout in the db
+
+router.post("/api/workouts", (req, res) => {
+    Workout.create(req.body)
+        .then((fitnesstrackerdb) => {
+            res.json(fitnesstrackerdb)
+        })
+        .catch((err) => {
+            res.json(err);
+        });
+});
+
+//add exercise to workout
+
+router.put("/api/workouts/:id", ({ params, body }, res => {
+    Workout.findOneandUpdate({ _id: params.id }, { $push: { exercise: body } }, { new: true })
+}))
